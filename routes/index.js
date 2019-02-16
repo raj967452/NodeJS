9460ca24aa470b9537a796a464ad7493c009fd65
DESKTOP-UA7RFN3\Admin <rajkumar967452@gmail.com>
2019-02-17 00:21:40 +0530

    handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Tutorial', condition:false });
+  res.render('index', { title: 'Express Tutorial', condition:false, anyArray: [1,2,3,4,5] });
 });
 /* GET home page. */
 router.get('/users', function(req, res, next) {
