6d4063a7c5df2d147d617bd9bd3290536008c6e7
DESKTOP-UA7RFN3\Admin <rajkumar967452@gmail.com>
2019-02-19 22:53:00 +0530

    get post

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Tutorial', condition:false, anyArray: [1,2,3,4,5] });
 });
 /* GET home page. */
-router.get('/users', function(req, res, next) {
+/*router.get('/users', function(req, res, next) {
   res.send('Respond with resource');
+});*/
+router.get('/user/:id', function(req, res, next) {
+  res.render('user id', {output: req.params.id});
 });
-/* GET home page. */
-router.get('/user/details', function(req, res, next) {
-  res.send('user details');
+router.post('/user/submit', function(req, res, next) {
+  var id = req.body.id;
+  res.redirect('/user/'+id);
 });
 module.exports = router;
